5619763ce9e8363d103aa02c61882ef7786b9dd7
Josh <lutz_143@yahoo.com>
2022-11-01 21:07:16 -0400

    added JavaScript for PopUp on Proprietary Work Projects

---
+++ assets/scripts/alert.js
@@ -0,0 +1,9 @@
+function sppectr_alert(){
+  let sppectrMessage = "Test Message here for SPPECTR";
+  window.alert(sppectrMessage);
+}
+
+function iDAD_alert(){
+  let sppectrMessage = "Test Message here for iDAD";
+  window.alert(sppectrMessage);
+}
