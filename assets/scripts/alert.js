323c0f19cf1872756d084b7e6f34ba6e40a8de96
Josh <lutz_143@yahoo.com>
2022-11-03 08:25:33 -0400

    updates to About Me and work links

--- assets/scripts/alert.js
+++ assets/scripts/alert.js
@@ -1,9 +1,14 @@
 function sppectr_alert(){
-  let sppectrMessage = "Test Message here for SPPECTR";
+  let sppectrMessage = "The Space Proposal Planning & EPP Counseling Tool Repository or SPPECTR, is a VBA based model that is intended to be a guide that navigates the various Corporate policy requirements by receiving proposal parameter inputs and producing an output of the leadership review level and functional engagement requirements.  This tool utilizes VBA code and extensive algorithms to determine the proposal’s requirements.  Here, I am the sole developer, tester, and administrator, which has refined my knowledge of VBA and project deployment techniques.  SPPECTR is a tool developed at work and its code/use is considered proprietary.";
   window.alert(sppectrMessage);
 }
 
 function iDAD_alert(){
-  let sppectrMessage = "Test Message here for iDAD";
+  let sppectrMessage = "The Independent Data Analytics Database or iDAD, is an Access-SQL database that I developed for the use of answering questions about program performance and profitability.  By creating this central repository, I was able to develop SQL queries to define programs of increased risk to the Corporation’s portfolio as well as programs that are performing ahead of plan.  As the sole developer, tester, and administrator, I continue to develop analytics within iDAD to support Corporate CFO requests every quarter.  iDAD is a tool developed at work and its code/use is considered proprietary.";
+  window.alert(sppectrMessage);
+}
+
+function phone_alert(){
+  let sppectrMessage = "Uh oh! I value internet privacy, feel free to reach out via email and we can share additional contact information.  Looking forward to connect!";
   window.alert(sppectrMessage);
 }
